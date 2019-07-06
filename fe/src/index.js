dd59a570e726870fec1ab8ca4cede015cbefced0
shogo takayama <shogo@chalkboard.me>
2019-07-06 12:02:16 +0900

    fe <=> be 通信.このままだとCORSで動かない

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,3 +1,16 @@
 import "./index.scss";
+import axios from 'axios';
 
 'use strict';
+
+window.onload = () => {
+  const element = document.querySelector("#page--index");
+
+  axios.get('http://localhost:4567/hello')
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+}
