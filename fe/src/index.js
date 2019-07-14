55889ef22ca3396a8566bc05b0cf752c012d52f4
shogo takayama <shogo@chalkboard.me>
2019-07-14 20:31:10 +0900

    新規作成処理、その結果をAjaxで画面反映

--- fe/src/index.js
+++ fe/src/index.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 'use strict';
 
 window.onload = () => {
-  const element = document.querySelector("#page--index");
   const mainElement = document.querySelector("#main");
   const template = document.querySelector("#template--task").content;
 
@@ -18,23 +17,45 @@ window.onload = () => {
       + `${String(dateTime.getSeconds()).padStart(2, '0')}`;
   }
 
+  const createTemplateCloneElement = (todo) => {
+    template.querySelector("[name=title]").value = todo['title'];
+    template.querySelector("[name=status]").value = todo['status'];
+    template.querySelector("[name=user]").value = todo['user_name'];
+    template.querySelector(".timestamp").innerText = formatDateTime(todo['updated_at']);
+
+    return document.importNode(template, true);
+  }
+
+  //初期画面描画
   axios.get('http://localhost:8080/todos') /* proxyの設定によりSinatraにアクセスが流れる */
   .then(function(response) {
     const data = response['data'];
     const fragment = document.createDocumentFragment();
     if(!data) return;
     data.forEach(todo => {
-      template.querySelector("[name=title]").value = todo['title'];
-      template.querySelector("[name=status]").value = todo['status'];
-      template.querySelector("[name=user]").value = todo['user_name'];
-      template.querySelector(".timestamp").innerText = formatDateTime(todo['updated_at']);
-
-      const clone = document.importNode(template, true);
+      const clone = createTemplateCloneElement(todo);
       fragment.appendChild(clone);
     }); 
     mainElement.appendChild(fragment);
   })
   .catch(function(error) {
     console.log(error);
+  });
+
+  // 新規作成formのイベント
+  document.querySelector("#new-todo-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const encodeFormData = new URLSearchParams(formData);
+
+    axios.post(event.target.action, encodeFormData)
+    .then((response) => {
+      const fragment = document.createDocumentFragment();
+      const todo = response['data'];
+      const clone = createTemplateCloneElement(todo);
+      fragment.appendChild(clone);
+      mainElement.appendChild(fragment);
+    });
   });
 }
